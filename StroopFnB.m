16aa7c95dcc64bfe87cd9ff0fce367021ae0c61a
Erik Knight <elk@uoregon.edu>
2015-01-12 13:53:44 -0800

    MultiTrial

--- StroopFnB.m
+++ StroopFnB.m
@@ -11,7 +11,7 @@ ID=str2double(answer{1});
 % prac = str2double(answer{4});
 
 
-rng(ID); %Seed random number generator with subject ID
+%rng(ID); %Seed random number generator with subject ID
 d = clock;
 
 
@@ -31,23 +31,32 @@ STIM = struct;
 STIM.rows = 10;
 STIM.cols = 10;
 STIM.totes = STIM.rows*STIM.cols;
+STIM.blocks = 1;
+STIM.trials = 2;
 
+%For saving later...
+[mdir,~,~] = fileparts(which('StroopFnB.m'));
+        
 commandwindow
 %% Word
 
 try
     load('stim_text.mat')
 catch
-    error('Cannot load stim_text file. Please be sure stim_text.mat is saved in the same directory as StroopFnB.m (this task''s file).')
+    try
+        cd(mdir);
+        load('stim_text.mat')
+    catch
+        error('Cannot load stim_text file. Please be sure stim_text.mat is saved in the same directory as StroopFnB.m (this task''s file).')
+    end
 end
 
 %Choose condition, word list, & color list;
-cond = CoinFlip(1,.5) + 1;  %1 = exp, 2 = control.
-word_cond = randi(4);
-color_cond = randi(4);
-wordlist = stim_text{word_cond,cond};
-colorlist =stim_text{color_cond,3};
+cond = randperm(2);  %1 = exp first, 2 = control first.
+word_cond = randi(4,2,1);      %randomly select one of four word lists from exp & control groups; # can repeat
+color_cond = randperm(4,2);     %randomly select one of four color lists; sample without replacement!
 
+Stroop = struct('ID',ID,'Date',sprintf('%s %2.0f:%02.0f',date,d(4),d(5)),'Condition',cond,'Word_List',word_cond,'Color_List',color_cond);
 
 
 %%
@@ -92,7 +101,21 @@ Screen('TextFont', w, 'Arial');
 %Screen('TextStyle', w, 1);
 Screen('TextSize',w,25);
 
-%% 
+%% Instructions!
+DrawFormattedText(w,'This is a test of how fast you can name the font color for the printed words. After pressing the start button, you are to go down the columns starting with the first one until you complete it and then continue without stopping down the remaining columns in order.  You will state aloud, the font color of the words.  If you make a mistake, correct your error and continue without stopping.\n\nAgain, you will name the font color as quickly and as accurately as possible ignoring the actual word typed.  When you have finished all columns press the space bar.\n\nPress any key to continue.','center','center',COLORS.WHITE,60,[],[],1.5);
+Screen('Flip',w);
+KbWait([],2);
+
+DrawFormattedText(w,'The task will begin now. Remember, name the COLOR of the font as quickly as you can. When you are finished, press the space bar.\n\nPress any key to begin the task.','center','center',COLORS.WHITE,60,[],[],1.5);
+Screen('Flip',w);
+KbWait([],2);
+
+%%
+%you can set the font sizes and styles here
+Screen('TextFont', w, 'Arial');
+%Screen('TextStyle', w, 1);
+Screen('TextSize',w,17);
+ 
 %figure out distance between words on present screen
 vert_loc = fix(wRect(4)/STIM.rows);
 horz_loc = fix(wRect(3)/STIM.cols);
@@ -100,362 +123,72 @@ horz_loc = fix(wRect(3)/STIM.cols);
 %produce equal intervals for word placement across vertical and horizontal
 %dimensions.
 wloc_vert = 35:vert_loc:vert_loc*STIM.rows;
-wloc_horz = 40:horz_loc:horz_loc*STIM.cols;
+wloc_horz = 20:horz_loc:horz_loc*STIM.cols;
 
-for x = 1:STIM.cols;
-    for y = 1:STIM.rows;
-        wcounter = (x-1)*STIM.rows + y;
-        dat_word = wordlist{wcounter};
-        CenterTextOnPoint(w,dat_word,wloc_horz(x),wloc_vert(y),wcolor(colorlist(wcounter),:));
-    end
-end
 
-rt_start = Screen('Flip',w); 
-
-FlushEvents();
-
-keypress = 0;
-
-while 1
-    [down, ~, code] = KbCheck();
+for trial = 1:STIM.trials;
+    wordlist = stim_text{Stroop.Word_List(trial),Stroop.Condition(trial)};
+    colorlist = stim_text{Stroop.Color_List(trial),3};
     
-    if down ==1 && any(find(code) == KEY.rt);
-%         keypress = keypress + 1;
-%         if keypress == 2;
-            rt = GetSecs() - rt_start;
+    
+    for x = 1:STIM.cols;
+        for y = 1:STIM.rows;
+            wcounter = (x-1)*STIM.rows + y;
+            dat_word = wordlist{wcounter};
+            %CenterTextOnPoint(w,dat_word,wloc_horz(x),wloc_vert(y),wcolor(colorlist(wcounter),:));
+            DrawFormattedText(w,dat_word,wloc_horz(x),wloc_vert(y),wcolor(colorlist(wcounter),:),10);
+        end
+    end
+    
+    rt_start = Screen('Flip',w,[],1);
+    
+    FlushEvents();
+    
+    while 1
+        Screen('Flip',w,[],1);
+        
+        [down, ~, code] = KbCheck();
+        
+        if down ==1 && any(find(code) == KEY.rt);
+            Stroop.rt(trial) = GetSecs() - rt_start;
             break
-%         end
+        end
+    end
+    
+    if trial < STIM.trials;
+        Screen('Flip',w);
+        DrawFormattedText(w,'Press any key to begin the next round.','center','center',COLORS.WHITE);
+        Screen('Flip',w);
+        KbWait([],2);
+        Screen('Flip',w);
+    end
+        
+end
+
+%% Save
+savedir = [mdir filesep 'Results' filesep];
+cd(savedir)
+savename = ['Stroop_' num2str(ID) '.mat'];
+
+if exist(savename,'file')==2;
+    savename = ['Stroop_' num2str(ID) '_' sprintf('%s_%2.0f%02.0f',date,d(4),d(5)) '.mat'];
+end
+
+try
+save([savedir savename],'Stroop');
+catch
+    warning('Something is amiss with this save. Retrying to save in a more general location...');
+    try
+        save([mdir filesep savename],'Stroop');
+    catch
+        warning('STILL problems saving....Try right-clicking on ''AAM'' and Save as...');
+        Stroop
     end
 end
 
 
+
+%% The End!
 sca
     
-
-
-
 end
- 
-%%
-function [nx, ny, textbounds] = CenterTextOnPoint(win, tstring, sx, sy,color)
-% [nx, ny, textbounds] = DrawFormattedText(win, tstring [, sx][, sy][, color][, wrapat][, flipHorizontal][, flipVertical][, vSpacing][, righttoleft])
-%
-% 
-
-numlines=1;
-
-if nargin < 1 || isempty(win)
-    error('CenterTextOnPoint: Windowhandle missing!');
-end
-
-if nargin < 2 || isempty(tstring)
-    % Empty text string -> Nothing to do.
-    return;
-end
-
-% Store data class of input string for later use in re-cast ops:
-stringclass = class(tstring);
-
-% Default x start position is left border of window:
-if isempty(sx)
-    sx=0;
-end
-
-% if ischar(sx) && strcmpi(sx, 'center')
-%     xcenter=1;
-%     sx=0;
-% else
-%     xcenter=0;
-% end
-
-xcenter=0;
-
-% No text wrapping by default:
-% if nargin < 6 || isempty(wrapat)
-    wrapat = 0;
-% end
-
-% No horizontal mirroring by default:
-% if nargin < 7 || isempty(flipHorizontal)
-    flipHorizontal = 0;
-% end
-
-% No vertical mirroring by default:
-% if nargin < 8 || isempty(flipVertical)
-    flipVertical = 0;
-% end
-
-% No vertical mirroring by default:
-% if nargin < 9 || isempty(vSpacing)
-    vSpacing = 1.5;
-% end
-
-% if nargin < 10 || isempty(righttoleft)
-    righttoleft = 0;
-% end
-
-% Convert all conventional linefeeds into C-style newlines:
-newlinepos = strfind(char(tstring), '\n');
-
-% If '\n' is already encoded as a char(10) as in Octave, then
-% there's no need for replacemet.
-if char(10) == '\n' %#ok<STCMP>
-   newlinepos = [];
-end
-
-% Need different encoding for repchar that matches class of input tstring:
-if isa(tstring, 'double')
-    repchar = 10;
-elseif isa(tstring, 'uint8')
-    repchar = uint8(10);    
-else
-    repchar = char(10);
-end
-
-while ~isempty(newlinepos)
-    % Replace first occurence of '\n' by ASCII or double code 10 aka 'repchar':
-    tstring = [ tstring(1:min(newlinepos)-1) repchar tstring(min(newlinepos)+2:end)];
-    % Search next occurence of linefeed (if any) in new expanded string:
-    newlinepos = strfind(char(tstring), '\n');
-end
-
-% % Text wrapping requested?
-% if wrapat > 0
-%     % Call WrapString to create a broken up version of the input string
-%     % that is wrapped around column 'wrapat'
-%     tstring = WrapString(tstring, wrapat);
-% end
-
-% Query textsize for implementation of linefeeds:
-theight = Screen('TextSize', win) * vSpacing;
-
-% Default y start position is top of window:
-if isempty(sy)
-    sy=0;
-end
-
-winRect = Screen('Rect', win);
-winHeight = RectHeight(winRect);
-
-% if ischar(sy) && strcmpi(sy, 'center')
-    % Compute vertical centering:
-    
-    % Compute height of text box:
-%     numlines = length(strfind(char(tstring), char(10))) + 1;
-    %bbox = SetRect(0,0,1,numlines * theight);
-    bbox = SetRect(0,0,1,theight);
-    
-    
-    textRect=CenterRectOnPoint(bbox,sx,sy);
-    % Center box in window:
-    [rect,dh,dv] = CenterRect(bbox, textRect);
-
-    % Initialize vertical start position sy with vertical offset of
-    % centered text box:
-    sy = dv;
-% end
-
-% Keep current text color if noone provided:
-if nargin < 5 || isempty(color)
-    color = [];
-end
-
-% Init cursor position:
-xp = sx;
-yp = sy;
-
-minx = inf;
-miny = inf;
-maxx = 0;
-maxy = 0;
-
-% Is the OpenGL userspace context for this 'windowPtr' active, as required?
-[previouswin, IsOpenGLRendering] = Screen('GetOpenGLDrawMode');
-
-% OpenGL rendering for this window active?
-if IsOpenGLRendering
-    % Yes. We need to disable OpenGL mode for that other window and
-    % switch to our window:
-    Screen('EndOpenGL', win);
-end
-
-% Disable culling/clipping if bounding box is requested as 3rd return
-% % argument, or if forcefully disabled. Unless clipping is forcefully
-% % enabled.
-% disableClip = (ptb_drawformattedtext_disableClipping ~= -1) && ...
-%               ((ptb_drawformattedtext_disableClipping > 0) || (nargout >= 3));
-% 
-
-disableClip=1;
-
-% Parse string, break it into substrings at line-feeds:
-while ~isempty(tstring)
-    % Find next substring to process:
-    crpositions = strfind(char(tstring), char(10));
-    if ~isempty(crpositions)
-        curstring = tstring(1:min(crpositions)-1);
-        tstring = tstring(min(crpositions)+1:end);
-        dolinefeed = 1;
-    else
-        curstring = tstring;
-        tstring =[];
-        dolinefeed = 0;
-    end
-
-    if IsOSX
-        % On OS/X, we enforce a line-break if the unwrapped/unbroken text
-        % would exceed 250 characters. The ATSU text renderer of OS/X can't
-        % handle more than 250 characters.
-        if size(curstring, 2) > 250
-            tstring = [curstring(251:end) tstring]; %#ok<AGROW>
-            curstring = curstring(1:250);
-            dolinefeed = 1;
-        end
-    end
-    
-    if IsWin
-        % On Windows, a single ampersand & is translated into a control
-        % character to enable underlined text. To avoid this and actually
-        % draw & symbols in text as & symbols in text, we need to store
-        % them as two && symbols. -> Replace all single & by &&.
-        if isa(curstring, 'char')
-            % Only works with char-acters, not doubles, so we can't do this
-            % when string is represented as double-encoded Unicode:
-            curstring = strrep(curstring, '&', '&&');
-        end
-    end
-    
-    % tstring contains the remainder of the input string to process in next
-    % iteration, curstring is the string we need to draw now.
-
-    % Perform crude clipping against upper and lower window borders for
-    % this text snippet. If it is clearly outside the window and would get
-    % clipped away by the renderer anyway, we can safe ourselves the
-    % trouble of processing it:
-    if disableClip || ((yp + theight >= 0) && (yp - theight <= winHeight))
-        % Inside crude clipping area. Need to draw.
-        noclip = 1;
-    else
-        % Skip this text line draw call, as it would be clipped away
-        % anyway.
-        noclip = 0;
-        dolinefeed = 1;
-    end
-    
-    % Any string to draw?
-    if ~isempty(curstring) && noclip
-        % Cast curstring back to the class of the original input string, to
-        % make sure special unicode encoding (e.g., double()'s) does not
-        % get lost for actual drawing:
-        curstring = cast(curstring, stringclass);
-        
-        % Need bounding box?
-%         if xcenter || flipHorizontal || flipVertical
-            % Compute text bounding box for this substring:
-            bbox=Screen('TextBounds', win, curstring, [], [], [], righttoleft);
-%         end
-        
-        % Horizontally centered output required?
-%         if xcenter
-            % Yes. Compute dh, dv position offsets to center it in the center of window.
-%             [rect,dh] = CenterRect(bbox, winRect);
-            [rect,dh] = CenterRect(bbox, textRect);
-            % Set drawing cursor to horizontal x offset:
-            xp = dh;
-%         end
-            
-%         if flipHorizontal || flipVertical
-%             textbox = OffsetRect(bbox, xp, yp);
-%             [xc, yc] = RectCenter(textbox);
-% 
-%             % Make a backup copy of the current transformation matrix for later
-%             % use/restoration of default state:
-%             Screen('glPushMatrix', win);
-% 
-%             % Translate origin into the geometric center of text:
-%             Screen('glTranslate', win, xc, yc, 0);
-% 
-%             % Apple a scaling transform which flips the direction of x-Axis,
-%             % thereby mirroring the drawn text horizontally:
-%             if flipVertical
-%                 Screen('glScale', win, 1, -1, 1);
-%             end
-%             
-%             if flipHorizontal
-%                 Screen('glScale', win, -1, 1, 1);
-%             end
-% 
-%             % We need to undo the translations...
-%             Screen('glTranslate', win, -xc, -yc, 0);
-%             [nx ny] = Screen('DrawText', win, curstring, xp, yp, color, [], [], righttoleft);
-%             Screen('glPopMatrix', win);
-%         else
-            [nx ny] = Screen('DrawText', win, curstring, xp, yp, color, [], [], righttoleft);
-%         end
-    else
-        % This is an empty substring (pure linefeed). Just update cursor
-        % position:
-        nx = xp;
-        ny = yp;
-    end
-
-    % Update bounding box:
-    minx = min([minx , xp, nx]);
-    maxx = max([maxx , xp, nx]);
-    miny = min([miny , yp, ny]);
-    maxy = max([maxy , yp, ny]);
-
-    % Linefeed to do?
-    if dolinefeed
-        % Update text drawing cursor to perform carriage return:
-        if xcenter==0
-            xp = sx;
-        end
-        yp = ny + theight;
-    else
-        % Keep drawing cursor where it is supposed to be:
-        xp = nx;
-        yp = ny;
-    end
-    % Done with substring, parse next substring.
-end
-
-% Add one line height:
-maxy = maxy + theight;
-
-% Create final bounding box:
-textbounds = SetRect(minx, miny, maxx, maxy);
-
-% Create new cursor position. The cursor is positioned to allow
-% to continue to print text directly after the drawn text.
-% Basically behaves like printf or fprintf formatting.
-nx = xp;
-ny = yp;
-
-% Our work is done. If a different window than our target window was
-% active, we'll switch back to that window and its state:
-if previouswin > 0
-    if previouswin ~= win
-        % Different window was active before our invocation:
-
-        % Was that window in 3D mode, i.e., OpenGL rendering for that window was active?
-        if IsOpenGLRendering
-            % Yes. We need to switch that window back into 3D OpenGL mode:
-            Screen('BeginOpenGL', previouswin);
-        else
-            % No. We just perform a dummy call that will switch back to that
-            % window:
-            Screen('GetWindowInfo', previouswin);
-        end
-    else
-        % Our window was active beforehand.
-        if IsOpenGLRendering
-            % Was in 3D mode. We need to switch back to 3D:
-            Screen('BeginOpenGL', previouswin);
-        end
-    end
-end
-
-return;
-end
-
